6834bc4dc7f4e88f0196e7cbc7be58f134f1ce98
nathanchu <63111210+nathanchu@users.noreply.github.com>
2026-02-28 12:17:23 -0500

    Update at 'Sat Feb 28 12:17:23 EST 2026'

--- src/main/java/frc/robot/auto/AutoWeekZero.java
+++ src/main/java/frc/robot/auto/AutoWeekZero.java
@@ -2,12 +2,16 @@ package frc.robot.auto;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.drive.SmoothMoveCommand;
 import frc.robot.intake.Intake;
 import frc.robot.intake.IntakeAutoCommand;
 import frc.robot.shooter.Shooter;
 
 public class AutoWeekZero {
   public static Command autoV1() {
-    return Commands.parallel(new IntakeAutoCommand(Intake.getInstance()), Shooter.getInstance().autoAimCommandAuto()).withDeadline(Commands.waitSeconds(20));
+    return Commands.sequence(
+        Commands.parallel(new IntakeAutoCommand(Intake.getInstance()), Shooter.getInstance().autoAimCommandAuto()).withDeadline(Commands.waitSeconds(10))
+        //X: 1.160m, Y: 4.448m,
+    );
   }
 }
