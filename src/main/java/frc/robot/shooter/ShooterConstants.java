2dddb1619d4b1b90b68ddedeb84f009e5f473900
nathanchu <63111210+nathanchu@users.noreply.github.com>
2026-02-28 12:09:55 -0500

    Update at 'Sat Feb 28 12:09:55 EST 2026'

--- src/main/java/frc/robot/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/shooter/ShooterConstants.java
@@ -42,7 +42,7 @@ public class ShooterConstants {
       case V2:
         motorToFlywheel = 1.0;
         flywheelToMotor = 1.0;
-        flywheelP = 0.0004;
+        flywheelP = 0.0003;
         flywheelI = 0.0;
         flywheelD = 0.0;
         flywheelV = 0.00015;
