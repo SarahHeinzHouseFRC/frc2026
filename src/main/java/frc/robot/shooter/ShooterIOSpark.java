f94c7edebead4127eeee0443d64796daaa11b207
nathanchu <63111210+nathanchu@users.noreply.github.com>
2026-04-09 16:30:56 -0500

    Update at 'Thu Apr 09 16:30:56 CDT 2026'

--- src/main/java/frc/robot/shooter/ShooterIOSpark.java
+++ src/main/java/frc/robot/shooter/ShooterIOSpark.java
@@ -98,7 +98,7 @@ public class ShooterIOSpark implements ShooterIO {
     baseFlywheelConfig.voltageCompensation(12.0);
     baseFlywheelConfig.smartCurrentLimit(40).idleMode(kCoast);
     baseFlywheelConfig.closedLoop.pid(flywheelP, flywheelI, flywheelD, ClosedLoopSlot.kSlot0);
-    baseFlywheelConfig.closedLoop.feedForward.kV(flywheelV, ClosedLoopSlot.kSlot0);
+    baseFlywheelConfig.closedLoop.feedForward.kV(0, ClosedLoopSlot.kSlot0);
     baseFlywheelConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder);
     baseFlywheelConfig.closedLoop.maxMotion.maxAcceleration(12000).allowedProfileError(1000);
     baseFlywheelConfig.openLoopRampRate(0.2);
@@ -230,7 +230,7 @@ public class ShooterIOSpark implements ShooterIO {
     flywheelController.setSetpoint(
         speedRotationsPerMinute,
         SparkBase.ControlType.kVelocity,
-        ClosedLoopSlot.kSlot0);
+        ClosedLoopSlot.kSlot0, speedRotationsPerMinute * v, SparkClosedLoopController.ArbFFUnits.kVoltage);
     //    double flywheelVelocity1 = motorToFlywheel * flywheelEncoder.getVelocity();
     //    double flywheelVelocity2 = motorToFlywheel * flywheelEncoder2.getVelocity();
     //    double flywheelVelocity = (flywheelVelocity1 + flywheelVelocity2) / 2;
