5def5523698f8ce529b64548f5e40e82a1aa16fc
nathanchu <63111210+nathanchu@users.noreply.github.com>
2026-04-10 09:23:25 -0500

    Update at 'Fri Apr 10 09:23:25 CDT 2026'

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -35,8 +35,8 @@ public class FieldConstants {
       HUB = fromInches(182.11, 158.845, 72);
       TRENCH_L = fromInches(0, 0, 0);
       TRENCH_R = fromInches(0, 0, 0);
-      SHOT_TARGET_R = fromInches(136, 119, 0);
-      SHOT_TARGET_L = fromInches(136, 198, 0);
+      SHOT_TARGET_R = fromInches(96, 119, 0);
+      SHOT_TARGET_L = fromInches(96, 198, 0);
       fieldWidth = 8.06933;
     } else if (TYPE == FieldType.ANDYMARK) {
       TOWER = fromInches(0, 0, 0);
@@ -47,8 +47,8 @@ public class FieldConstants {
       HUB = fromInches(181.56, 158.32, 72);
       TRENCH_L = fromInches(0, 0, 0);
       TRENCH_R = fromInches(0, 0, 0);
-      SHOT_TARGET_R = fromInches(136, 119, 0);
-      SHOT_TARGET_L = fromInches(136, 198, 0);
+      SHOT_TARGET_R = fromInches(96, 119, 0);
+      SHOT_TARGET_L = fromInches(96, 198, 0);
       fieldWidth = 8.04266;
     } else {
       throw new IllegalStateException("Invalid field type");
