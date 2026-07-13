5fe4db4a33bdba76684cdbef9806f3b93dbedbc2
nathanchu <63111210+nathanchu@users.noreply.github.com>
2026-07-13 12:09:58 -0400

    add second pivot motor

--- src/main/java/frc/robot/subsystems/ball/Ball.java
+++ src/main/java/frc/robot/subsystems/ball/Ball.java
@@ -30,6 +30,8 @@ public class Ball extends SubsystemBase {
   private final SparkClosedLoopController intakeController;
   private final SparkMax pivotMotor = new SparkMax(26, SparkLowLevel.MotorType.kBrushless);
   private final SparkClosedLoopController pivotController;
+  private final SparkMax pivotMotor2 = new SparkMax(27, SparkLowLevel.MotorType.kBrushless);
+  private final SparkClosedLoopController pivotController2;
 
   private final SparkFlex shooterMotor = new SparkFlex(32, SparkLowLevel.MotorType.kBrushless);
   private final SparkFlex shooterMotor2 = new SparkFlex(33, SparkLowLevel.MotorType.kBrushless);
@@ -90,7 +92,7 @@ public class Ball extends SubsystemBase {
     intakeController = intakeMotor.getClosedLoopController();
 
     SparkMaxConfig pivotConfig = new SparkMaxConfig();
-    pivotConfig.smartCurrentLimit(40).idleMode(SparkBaseConfig.IdleMode.kCoast).inverted(false);
+    pivotConfig.smartCurrentLimit(40).idleMode(SparkBaseConfig.IdleMode.kCoast);
     pivotConfig.absoluteEncoder.inverted(false);
     pivotConfig.inverted(true);
     pivotConfig.closedLoop.pid(
@@ -120,6 +122,13 @@ public class Ball extends SubsystemBase {
         pivotConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     pivotController = pivotMotor.getClosedLoopController();
 
+    SparkMaxConfig pivotConfig2 = new SparkMaxConfig();
+    pivotConfig2.apply(pivotConfig);
+    pivotConfig2.absoluteEncoder.inverted(true);
+    pivotConfig2.inverted(true);
+
+    pivotMotor2.configure(pivotConfig2, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    pivotController2 = pivotMotor2.getClosedLoopController();
   }
 
   private double targetFlywheelSpeed = 0;
@@ -154,10 +163,12 @@ public class Ball extends SubsystemBase {
 
   public void setIntakePosition(double position) {
     pivotController.setSetpoint(position, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0);
+    pivotController2.setSetpoint(position, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0);
   }
 
   public void setIntakePositionSlowly(double position) {
     pivotController.setSetpoint(position, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot1);
+    pivotController2.setSetpoint(position, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot1);
   }
 
   public void stopIntake() {
