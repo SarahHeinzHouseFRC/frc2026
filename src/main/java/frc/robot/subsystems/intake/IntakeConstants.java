c637fd81bc4b642d91a77c5ab040cb9dcea7a388
nathanchu <63111210+nathanchu@users.noreply.github.com>
2026-07-22 19:42:35 -0400

    update IntakeConstants wrt new zeroing procedure

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -4,6 +4,6 @@ public class IntakeConstants {
   public static final double overBumperPivotP = 1.0;
   public static final double overBumperPivotI = 0.0;
   public static final double overBumperPivotD = 0.0;
-  public static final double presetStowed = 0.550;
-  public static final double presetEngaged = 0.07;
+  public static final double presetStowed = 0.60;
+  public static final double presetEngaged = 0.12;
 }
